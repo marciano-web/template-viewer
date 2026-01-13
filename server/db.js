62ec4982f3a37092fc060dcfdac9ba716856695a
marciano-web <marcianofmarques@hotmail.com>
2026-01-12 23:48:33 -0300

    Delete server directory

--- server/db.js
+++
@@ -1,4 +0,0 @@
-import pg from 'pg';
-const {Pool}=pg;
-export const pool=new Pool({connectionString:process.env.DATABASE_URL,ssl:{rejectUnauthorized:false}});
-export async function initDb(){await pool.query("CREATE EXTENSION IF NOT EXISTS pgcrypto;");}
